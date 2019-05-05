684a9ca1c9bca8f36e9dffd32a58021e15d37be1
bevexed <547551650@qq.com>
2019-05-05 16:12:04 +0800

    登录 注册 页面

---
+++ login/base-information.vue
@@ -0,0 +1,126 @@
+<template>
+    <view class="base-information">
+        <view class="title">手机验证登录</view>
+        <view class="form">
+            <input type="text" v-model="name" placeholder="真实姓名" placeholder-style="color:#aaaaaa;font-weight:300;font-size:16px" />
+            <input type="text" v-model="company" placeholder="公司名称" placeholder-style="color:#aaaaaa;font-weight:300;font-size:16px" />
+            <input type="text" v-model="occupation" placeholder="您的职务" placeholder-style="color:#aaaaaa;font-weight:300;font-size:16px" />
+        </view>
+
+        <view class="agreement">
+            <view :class="['select', { active: agreement }]" @tap="agreement = !agreement"><view class="selected"></view></view>
+            <view>
+                <text class="star"></text>
+                已阅读并同意购物条款并了解
+                <text class="href">隐私政策</text>
+            </view>
+        </view>
+
+        <view :class="['pay-button', { active: agreement && name.length > 0 && occupation.length > 0 && company.length > 0 }]" @click="toHome">确认</view>
+    </view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: '',
+            company: '',
+            occupation: '',
+            // 是否同意
+            agreement: false
+        };
+    },
+    methods: {
+        toHome() {
+            let { name, company, occupation, agreement } = this;
+            if (agreement && name.length > 0 && occupation.length > 0 && company.length > 0) {
+                uni.switchTab({
+                    url: '../pages/home/home'
+                });
+            }
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+.base-information {
+    padding: 20upx 40upx;
+    .title {
+        font-size: 32upx;
+        font-family: PingFang-SC-Bold;
+        font-weight: bold;
+        color: #333;
+        text-align: center;
+    }
+
+    .form {
+        margin-top: 256upx;
+    }
+
+    input {
+        display: flex;
+        align-items: center;
+        margin-top: 90upx;
+        padding: 14upx 0;
+        border-bottom: 2upx solid #eeeeee;
+    }
+
+    .agreement {
+        display: flex;
+        align-items: center;
+        font-size: 20upx;
+        color: #666;
+        margin-top: 28upx;
+        .select {
+            margin-right: 20upx;
+        }
+        .star {
+            font-weight: bold;
+            color: #c50000;
+        }
+
+        .href {
+            color: #333;
+            font-weight: 500;
+            text-decoration: underline;
+        }
+    }
+
+    .select {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 30upx;
+        height: 30upx;
+        border-radius: 50%;
+        border: 1px solid rgba(204, 204, 204, 1);
+        &.active {
+            border-color: $theme-color;
+            .selected {
+                width: 18upx;
+                height: 18upx;
+                background: $theme-color;
+                border-radius: 50%;
+            }
+        }
+    }
+
+    .pay-button {
+        $height: 80upx;
+        height: $height;
+        line-height: $height;
+        width: 692upx;
+        margin: 226upx auto 68upx;
+        text-align: center;
+        background: #cccccc;
+        border-radius: 8upx;
+        color: #fff;
+        font-size: 28upx;
+        &.active {
+            background: $theme-color;
+        }
+    }
+}
+</style>
---
+++ login/login.vue
@@ -0,0 +1,204 @@
+<template>
+    <view class="login">
+        <view class="title">手机验证登录</view>
+
+        <view class="form">
+            <view class="phone">
+                <image src="../static/icon/phone.svg" mode=""></image>
+                <input type="number" v-model="phone" maxlength="11" placeholder="请输入11位电话号码" placeholder-style="color:#aaaaaa;font-weight:300;font-size:16px" />
+            </view>
+            <view class="code">
+                <image src="../static/icon/AK.svg" mode=""></image>
+                <input type="password" v-model="code" placeholder="请输入验证码" placeholder-style="color:#cccccc;font-weight:300;font-size:16px" />
+                <view :class="['button', { active: phone.length === 11 && !send }, { sended: send }]" @tap="sendMsg">{{ send ? '重新获取(' + time + 's)' : '获取验证码' }}</view>
+            </view>
+        </view>
+
+        <view class="agreement">
+            <view :class="['select', { active: agreement }]" @tap="agreement = !agreement"><view class="selected"></view></view>
+            <view>
+                <text class="star"></text>
+                已阅读并同意购物条款并了解
+                <text class="href">隐私政策</text>
+            </view>
+        </view>
+
+        <view :class="['pay-button', { active: agreement && phone.length === 11 && code.length >= 4 }]" @click="toBaseInformation">登录</view>
+    </view>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            // 是否同意
+            agreement: false,
+            // 电话号
+            phone: '',
+            // 验证码
+            code: '',
+            // 是否 已发送 短信
+            send: false,
+            // 倒计时
+            time: 5,
+            // 定时器，
+            timer: null
+        };
+    },
+    computed: {},
+    methods: {
+        sendMsg() {
+            let { phone, send, time, timer } = this;
+            if (!phone) {
+                return;
+            }
+
+            if (phone.length < 11) {
+                return;
+            }
+
+            if (send) {
+                return;
+            }
+
+            if (timer) {
+                return;
+            }
+
+            this.send = true;
+            this.timer = setInterval(() => {
+                time--;
+                this.time = time;
+                if (time === 0) {
+                    this.time = 60;
+                    this.send = false;
+                    clearInterval(this.timer);
+                    this.timer = null;
+                }
+            }, 1000);
+        },
+        toBaseInformation() {
+            let { phone, code, agreement } = this;
+            if (agreement && phone.length === 11 && code.length >= 4) {
+                uni.navigateTo({
+                    url:'base-information'
+                })
+            }
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.login {
+    padding: $white-space;
+    .title {
+        font-size: 32upx;
+        font-family: PingFang-SC-Bold;
+        font-weight: bold;
+        color: #333;
+        text-align: center;
+    }
+
+    .form {
+        margin-top: 256upx;
+    }
+
+    .phone,
+    .code {
+        display: flex;
+        align-items: center;
+        margin-top: 100upx;
+        padding: 14upx 0;
+        border-bottom: 2upx solid #eeeeee;
+    }
+
+    input {
+        flex: 1;
+    }
+
+    .button {
+        width: 162upx;
+        height: 52upx;
+        line-height: 52upx;
+        background: rgba(170, 170, 170, 0.3);
+        color: #fff;
+        font-size: 24upx;
+        border-radius: 8upx;
+        text-align: center;
+        border: 2upx solid transparent;
+        padding: 0 12upx;
+        &.active {
+            background: $theme-color;
+        }
+
+        &.sended {
+            background: #fff;
+            color: $theme-color;
+            border: 2upx solid $theme-color;
+        }
+    }
+
+    image {
+        width: 36upx;
+        height: 36upx;
+        margin-right: 20upx;
+    }
+
+    .agreement {
+        display: flex;
+        align-items: center;
+        font-size: 20upx;
+        color: #666;
+        margin-top: 28upx;
+        .select {
+            margin-right: 20upx;
+        }
+        .star {
+            font-weight: bold;
+            color: #c50000;
+        }
+
+        .href {
+            color: #333;
+            font-weight: 500;
+            text-decoration: underline;
+        }
+    }
+
+    .select {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 30upx;
+        height: 30upx;
+        border-radius: 50%;
+        border: 1px solid rgba(204, 204, 204, 1);
+        &.active {
+            border-color: $theme-color;
+            .selected {
+                width: 18upx;
+                height: 18upx;
+                background: $theme-color;
+                border-radius: 50%;
+            }
+        }
+    }
+
+    .pay-button {
+        $height: 80upx;
+        height: $height;
+        line-height: $height;
+        width: 692upx;
+        margin: 226upx auto 68upx;
+        text-align: center;
+        background: #cccccc;
+        border-radius: 8upx;
+        color: #fff;
+        font-size: 28upx;
+        &.active {
+            background: $theme-color;
+        }
+    }
+}
+</style>
